dda11930291ad93f2ebee296df4dce32eb640ba0
AJackTi <AJackTi@AJACKTI>
2020-03-13 11:43:41 +0700

    Add Category and SubCategory

---
+++ Spice/Spice/Areas/Admin/Controllers/SubCategoryController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Spice.Data;
+using Spice.Models;
+
+namespace Spice.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class SubCategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public SubCategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        //Get INDEX
+        public async Task<IActionResult> Index()
+        {
+            var subCategories = await _db.SubCategory.Include(s => s.Category).ToListAsync();
+            return View(subCategories);
+        }
+
+        // GET - CREATE
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST - CREATE
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(SubCategory subCategory)
+        {
+            if (ModelState.IsValid)
+            {
+                //if valid
+                _db.SubCategory.Add(subCategory);
+                await _db.SaveChangesAsync();
+
+                return RedirectToAction(nameof(Index));
+            }
+            return View(subCategory);
+        }
+    }
+}
